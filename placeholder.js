68605db4fe563fd5edad23690ac294b8abeeeaae
zhangjunmin <zhangjunmin@wedocare.com>
2014-10-31 17:08:29 +0800

    'modify'

--- placeholder.js
+++ placeholder.js
@@ -25,7 +25,6 @@
 				(function(span,input){
 					var $input = $(input);
 					$input.before(span);
-					$input.parent().css('position','relative');
 					span.innerText = $input.attr('placeholder');
 
 					$(span).css({
